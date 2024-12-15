a45d86d342dcba2588d4aa7b64900826351e56c9
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-12-15 17:14:40 +0530

    Sign In and Sign Up Page UI

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,8 +1,10 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function Login() {
+  const router = useRouter();
   return (
     <View>
       <Image source={require('./../assets/images/login.jpg')}
@@ -25,9 +27,9 @@ export default function Login() {
             color: Colors.GRAY,
             marginTop: '20%'
         }}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI-driven insights.</Text>
-        <View style={styles.button}>
-            <Text style={{color: Colors.WHITE, textAlign: 'center', fontFamily: 'outfit', fontSize: 17}}>Sign In With Google</Text>    
-        </View>        
+        <TouchableOpacity onPress={() => router.push('auth/sign-in')} style={styles.button}>
+            <Text style={{color: Colors.WHITE, textAlign: 'center', fontFamily: 'outfit', fontSize: 17}}>Get Started</Text>    
+        </TouchableOpacity>        
       </View>
     </View>
   )
